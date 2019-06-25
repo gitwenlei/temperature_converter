3bac082b1c7368761774e19a7971365f611c64aa
gitwenlei <ngwenlei@gmail.com>
2019-06-26 00:06:56 +0800

    Added isANumber checking function using regexp

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+// STEP 1
+// Display temperature from user input
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+//   display( currentInput );
+// };
+
+// STEP 2
+// Display temperature from user input with number checking
+// Check if input is number
+var isANumber = function(currentInput) {
+    return !/\D/.test(currentInput); //return true or false
 };
+
+var inputHappened = function(currentInput) {
+    // if it is number
+    if (isANumber(currentInput)) {
+        // display number entered
+        var output = currentInput;
+    } else {
+        var output = "Please enter numbers only."
+    }
+    display (output);
+};
+
+
+// Part 1
+// Prompt the user for a starting temperature. This should be a numerical value that represents degrees in fahrenheit.
+
+// After receiving input it should show that same temperature in both Kelvin and Celsius.
