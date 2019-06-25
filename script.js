404b8c9ee69c0759b550740592b61845d151dd9f
gitwenlei <ngwenlei@gmail.com>
2019-06-26 00:35:55 +0800

    Added temperature in Kelvin formula & convertToKelvin function

--- script.js
+++ script.js
@@ -14,17 +14,23 @@ var isANumber = function(currentInput) {
     return !/\D/.test(currentInput); //return true or false
 };
 
+var convertToKelvin = function (currentInput) {
+    var temperatureKelvin = (currentInput-32)/1.8;
+}
+
 var inputHappened = function(currentInput) {
+    var parsedInput = parseInt(currentInput)
+
     // if it is number
-    if (isANumber(currentInput)) {
+    if (isANumber(parsedInput)) {
         // display number entered
-        var output = currentInput;
+        //var output = currentInput;
+        var output = parsedInput + " " + "oC" + " = " + (Math.round((parsedInput-32)/1.8) + " " + "oF";
     } else {
         var output = "Please enter numbers only."
     }
     display (output);
 };
-
 
 // Part 1
 // Prompt the user for a starting temperature. This should be a numerical value that represents degrees in fahrenheit.
